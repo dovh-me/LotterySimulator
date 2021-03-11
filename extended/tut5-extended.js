ca306844d1898681cfced8ed3ac607fa2b4faac6
dovh-me <dovh.me@gmail.com>
2021-03-11 07:54:46 +0530

    Completes Extended Version [Needs do add some more validations]

--- extended/tut5-extended.js
+++ extended/tut5-extended.js
@@ -1,42 +1,90 @@
-var usernum1 = document.getElementById("usernum1");
-var usernum2 = document.getElementById("usernum2");
-var usernum3 = document.getElementById("usernum3");
-var usernum4 = document.getElementById("usernum4");
-var usernum5 = document.getElementById("usernum5");
-var usernum6 = document.getElementById("usernum6");
+// Didn't use prompt cause its boring
 
-var drawNumbers = [];
-var bonusNum;
-for(var i = 1; i <= 7;){
-    var lotteryNum = Math.floor(Math.random() * 59) + 1;
-    if(!drawNumbers.includes(lotteryNum)){
+var userNumFields = [
+    document.getElementById("num1"),
+    document.getElementById("num2"),
+    document.getElementById("num3"),
+    document.getElementById("num4"),
+    document.getElementById("num5"),
+    document.getElementById("num6")
+];
 
-        if(i === 7) bonusNum = lotteryNum;
-        else drawNumbers.push(lotteryNum);
-        
-        var elementName = "drawnum" + parseInt(i);
-        lotteryNum = lotteryNum < 10? "0" + lotteryNum : lotteryNum;
-        document.getElementById(elementName).innerHTML = lotteryNum;
-        i++;
+const lotteryButtons = [
+    document.getElementById("drawnum1"),
+    document.getElementById("drawnum2"),
+    document.getElementById("drawnum3"),
+    document.getElementById("drawnum4"),
+    document.getElementById("drawnum5"),
+    document.getElementById("drawnum6"),
+];
+
+const userButtons  = [
+    document.getElementById("usernum1"),
+    document.getElementById("usernum2"),
+    document.getElementById("usernum3"),
+    document.getElementById("usernum4"),
+    document.getElementById("usernum5"),
+    document.getElementById("usernum6"),
+];
+
+
+var userNums = [];
+const validateBtn = document.getElementById("validateBtn");
+const drawBtn = document.getElementById("drawLottery");
+
+validateBtn.addEventListener("click", isNumsValid);
+drawBtn.addEventListener("click", drawLottery);
+
+
+function randonNumGen(){
+    var drawNumbers = [];
+    for(var i = 1; i <= 8;i++){
+        var lotteryNum = Math.floor(Math.random() * 59) + 1;
+        if(!drawNumbers.includes(lotteryNum)) drawNumbers.push(lotteryNum);
     }
+    return drawNumbers;
 }
 
-var usernums = [usernum1,usernum2,usernum3,usernum4,usernum5,usernum6];
-
-usernums.forEach((element) => {
-    if (drawNumbers.includes(parseInt(element.innerHTML))){
-        element.classList.add("highlightWin");
-    }else if (element.innerHTML == bonusNum){
-        element.classList.add("highlightBonusWin");
+function isNumsValid(){
+    var isValid;
+    var correctFields = 0;
+    for (var i = 0; i < userNumFields.length; i++) {
+        var userNum = parseInt(userNumFields[i].value);
+        if(userNums.includes(userNum) || isNaN(userNum) || userNum < 1 || userNum > 59){
+            isValid = false; 
+            var input = parseInt(userNumFields[i].value);
+            userNumFields[i].style.border = "2px solid red";
+            userNumFields[i].style.backgroundColor = "#ffcccb";
+        }else{
+            userNumFields[i].style.border = "2px solid green";
+            userNumFields[i].style.backgroundColor = "#90EE90";
+            correctFields++;
+        }
+        userNums[i] = userNum;
     }
-});
-
-var checkNumsButton = document.getElementById("validateBtn");
-
-checkNumsButton.addEventListener("click", () =>{
-    var inputNum = [];
-    for(var i = 1; i < 7; i++) {
-        var elementName = "num1" + i;
-        inputNum.push(document.getElementById(elementName)); 
+    
+    if(correctFields === userNumFields.length){
+        drawBtn.removeAttribute("disabled");
+    }else{
+        userNums = [];
     }
-});
+
+}
+
+function drawLottery(){
+    document.getElementById("lotteryForm").style.display = "none";
+    document.getElementById("lotteryContainer").style.display = "block";
+    const lotteryNums = randonNumGen();
+    console.log(lotteryNums);
+    bonusNum = lotteryNums.pop();
+    for(var i = 0; i < userNums.length; i++){
+        lotteryButtons[i].innerHTML = lotteryNums[i] > 9 ?lotteryNums[i]: "0" + lotteryNums[i];
+        userButtons[i].innerHTML = userNums[i] > 9 ?userNums[i]: "0" + userNums[i];
+        if(lotteryNums.includes(userNums[i])){
+            userButtons[i].classList.add("highlightWin");
+        }else if(userNums[i] == bonusNum){
+            userButtons[i].classList.add("highlightBonusWin");
+        }
+    }
+    document.getElementById("drawnum7").innerHTML = bonusNum > 9 ? bonusNum: "0" + bonusNum;
+}
